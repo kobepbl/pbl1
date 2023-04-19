cfa261cc2c610f52ef45111e43f63491fae888e6
GotouAsahi <112850894+GotouAsahi@users.noreply.github.com>
2023-04-19 12:52:33 +0900

    sqlエラー修正

--- sql/comment.sql
+++ sql/comment.sql
@@ -3,9 +3,9 @@
 CREATE TABLE comment_list(
     comment_id int PRIMARY KEY AUTO_INCREMENT,
     comment VARCHAR(200),
-    article_id,
+    article_id int NOT NULL,
     posted_date DATETIME NOT NULL,
-    CREATE INDEX comment_list_index(
+    INDEX comment_list_index(
         comment_id,
         comment
     )
