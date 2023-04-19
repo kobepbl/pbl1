6144232dab89bf7e28b242594185f93a160afdf1
bokudayoboku <kd1329183@st.konedenshi.ac.jp>
2023-04-19 11:58:16 +0900

    comment

--- comment.sql
+++ comment.sql
@@ -6,7 +6,20 @@ CREATE TABLE comment_list(
     article_id,
     posted_date DATETIME NOT NULL,
     CREATE INDEX comment_list_index(
-        
-
+        comment_id,
+        comment
     )
-)
+);
+
+-- コメントの確認
+SELECT
+    cmment
+FROM
+    comment_list
+GROUP BY
+    article_id
+HAVING
+    条件があるなら指定
+ORDER BY
+    ソート指定
+;
