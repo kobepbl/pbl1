42b8cf182679e31be5025e152ae0eab26e548990
GotouAsahi <112850894+GotouAsahi@users.noreply.github.com>
2023-04-19 21:30:55 +0900

    記事一覧と記事詳細完成

--- header.php
+++ header.php
@@ -1,24 +1,28 @@
+<?php
+require_once __DIR__ . '/pre.php';
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
   <meta charset="UTF-8">
   <title>神戸電子情報共有サイト</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="<?= $style_css ?>">
 </head>
 
 <body>
   <!-- header部分 -->
   <header>
     <div class="top-info">
-      <a href="index.html">
+      <a href="<?= $index_php ?>">
         <h1>神戸電子情報共有サイト</h1>
       </a>
     </div>
     <nav>
       <ul class="nav-list">
         <li class="nav-list-item">
-          <a href="toukou.html">投稿</a>
+          <a href="post_php">投稿</a>
         </li>
         <li class="nav-list-item">
           <a href="login.html">ログイン</a>
