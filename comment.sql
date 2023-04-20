5d038576d995dab5cce025e8e82320deb6b74ef6
bokudayoboku <kd1329183@st.konedenshi.ac.jp>
2023-04-20 09:31:57 +0900

    comment

--- comment.sql
+++ comment.sql
@@ -6,7 +6,7 @@ CREATE TABLE article_comment_list(
     article_id int NOT NULL,
     user_id int NOT NULL,
     comment VARCHAR(400) NOT NULL,
-    posted_date DATETIME NOT NULL
+    posted_date DATETIME NOT NULL,
     INDEX article_comment_list_index(
         comment_id,
         posted_date
@@ -20,11 +20,11 @@ CREATE TABLE question_comment_list(
     question_id int NOT NULL,
     user_id int NOT NULL,
     comment VARCHAR(400) NOT NULL,
-    posted_date DATETIME NOT NULL
+    posted_date DATETIME NOT NULL,
     INDEX article_comment_list_index(
         comment_id,
         posted_date
     ),
     FOREIGN KEY (user_id) REFERENCES current_users(user_id),
-    REFERENCES KEY (question_id) REFERENCES question_list(question_id)
+    FOREIGN KEY (question_id) REFERENCES question_list(question_id)
 );
