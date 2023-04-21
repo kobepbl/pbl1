664780ddf6753c6ff10452e0416f75cc34ce9d5a
UemuraKatsuki <kd1330249@st.kobedenshi.ac.jp>
2023-04-21 13:08:59 +0900

    質問ページング

--- paging/paging.php
+++ paging/paging.php
@@ -1,7 +1,7 @@
 <p class="from_to"><?php echo $count; ?>件中 <?php echo $from_record; ?> - <?php echo $to_record; ?> 件目を表示</p>
 <div class="pagination">
   <?php if ($page > 1) : ?>
-    <a href="index.php?page_id=<?php echo ($page - 1); ?>" class="page_feed">&laquo;</a>
+    <a href="index.php?a_page_id=<?php echo ($page - 1); ?>&q_page_id=<?php echo ($q_page); ?>" class="page_feed">&laquo;</a>
   <?php else :; ?>
     <span class="first_last_page">&laquo;</span>
   <?php endif; ?>
@@ -10,12 +10,12 @@
       <?php if ($i == $page) : ?>
         <span class="now_page_number"><?php echo $i; ?></span>
       <?php else : ?>
-        <a href=?page_id=<?php echo $i; ?> class="page_number"><?php echo $i; ?></a>
+        <a href=?a_page_id=<?php echo $i; ?>&q_page_id=<?php echo $q_page; ?> class="page_number"><?php echo $i; ?></a>
       <?php endif; ?>
     <?php endif; ?>
   <?php endfor; ?>
   <?php if ($page < $max_page) : ?>
-    <a href="index.php?page_id=<?php echo ($page + 1); ?>" class="page_feed">&raquo;</a>
+    <a href="index.php?a_page_id=<?php echo ($page + 1); ?>&q_page_id=<?php echo ($q_page); ?>" class="page_feed">&raquo;</a>
   <?php else : ?>
     <span class="first_last_page">&raquo;</span>
   <?php endif; ?>
