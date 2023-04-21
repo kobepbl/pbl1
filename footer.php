6f29385600a6e9b6260dcba132e45c174a7987fb
Ishii Yuuki <kd1329330@st.kobedenshi.ac.jp>
2023-04-21 10:34:48 +0900

    投稿

--- footer.php
+++ footer.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <!-- footer部分 -->
 <footer>
   <p class="footer-text">フッターだぜ</p>
 </footer>
 </body>
 
-=======
-<!-- footer部分 -->
-<footer>
-  <p class="footer-text">フッターだぜ</p>
-</footer>
-</body>
-
->>>>>>> 6b023fbe301576bb6d9010692e2b64e2bc4f00d5
 </html>
--- header.php
+++ header.php
@@ -30,20 +30,13 @@ require_once __DIR__ . '/pre.php';
     <nav>
       <ul class="nav-list">
         <li class="nav-list-item">
-<<<<<<< HEAD
           <a href="$post_php">投稿</a>
-=======
-          <a href="post_php">投稿</a>
->>>>>>> 6b023fbe301576bb6d9010692e2b64e2bc4f00d5
         </li>
         <?php
         if (!$_SESSION["is_login"]) {
           echo '<li class="nav-list-item"><a href="' . $signup_php . '">新規登録</a></li>';
           echo '<li class="nav-list-item"><a href="' . $login_php . '">ログイン</a></li>';
-<<<<<<< HEAD
           echo '<li class="nav-list-item"><a href="' . $post_php . '">投稿</a></li>';
-=======
->>>>>>> 6b023fbe301576bb6d9010692e2b64e2bc4f00d5
         } else {
           // echo '<li class="nav-list-item"><a href="' . $user_php . '">マイページ</a></li>';
           // echo '<li class="nav-list-item"><a href="' . $user_php . '">投稿</a></li>';
