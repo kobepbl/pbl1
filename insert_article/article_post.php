ab34f6ad816282db4b56a42df34e6d77597f5e31
Ishii Yuuki <kd1329330@st.kobedenshi.ac.jp>
2023-04-24 13:27:17 +0900

    投稿

--- insert_article/article_post.php
+++ insert_article/article_post.php
@@ -22,19 +22,18 @@ $userId = isset($_SESSION['userId']) ? $_SESSION['userId'] : '';
 <form class="form" method="POST" action="./article_db.php">
 
     <input type="hidden" name="userid" value="1"> 
+    
     <div class="item">
-        <fieldset>
-            <legend><br><label for="num">タイトル</label></legend>
-            <input type="text" name="title" id="num" placeholder="タイトルを入力" maxlength ="30" required>
-        </fieldset>
+            <label class="label_left" for="num">タイトル</label>
+            <input class="form-text" type="text" name="title" id="num" placeholder="タイトルを入力" maxlength ="30" required><br>        
     </div>
+    
     <div class="item">
-        <fieldset>
-                <legend><label for="num1">本文</label></legend>
-                <input type="text" name="sentence" id="num1"placeholder="本文を入力" maxlength ="400" required>
-            </fieldset>
+            <label class="label_left" for="num1">本文</label>
+            <input class="form-text" type="text" name="sentence" id="num1"placeholder="本文を入力" maxlength ="400" required>
     </div>
+    
     <div class="item">
-        <input type="submit" value="送信"><input type="reset" value="リセット">
+        <input type="submit" value="送信" class="button"><input type="reset" value="リセット" class="button">
     <div class="item">
 </form>
