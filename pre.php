ea3ddf7ba8d5328c1fe07ef68b6a3efba4f7541e
Ishii Yuuki <kd1329330@st.kobedenshi.ac.jp>
2023-04-25 11:38:44 +0900

    投稿

--- pre.php
+++ pre.php
@@ -4,18 +4,16 @@ if (!isset($_SESSION)) {
 }
 
 $name = isset($_SESSION['name']) ? $_SESSION['name'] : '';
-$user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : '';
-if (empty($use_id) || empty($name)) {
-  if (isset($_COOKIE['user_id']) && isset($_COOKIE['name'])) {
+
+if (empty($name)) {
+  if (isset($_COOKIE['name'])) {
     $name = $_COOKIE['name'];
-    $user_id = $_COOKIE["user_id"];
+
   } else {
     $name = 'no_login';
     setcookie("name", $name, time() + 60 * 60 * 24 * 14, '/');
-    setcookie("user_id", $user_id, time() + 60 * 60 * 24 * 14, '/');
   }
   $_SESSION['name'] = $name;
-  $_SESSION['user_id'] = $user_id;
 }
 
 
