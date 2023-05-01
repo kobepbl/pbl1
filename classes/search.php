22e4a3bd7afcaad6702cc06c886c1308a6d229cb
GotouAsahi <112850894+GotouAsahi@users.noreply.github.com>
2023-05-01 19:10:17 +0900

    sql改善

--- classes/search.php
+++ classes/search.php
@@ -8,10 +8,29 @@ class  Search  extends  DbData
   // 検索されたすべての記事を逆順でを取り出す
   public  function  getSearcharticles($search_word)
   {
-    $sql  = "select  *  from  article_list join current_users on article_list.user_id = current_users.user_id where sentence like '" . "%" . $search_word . "%" . "' order by article_list.article_id desc";
+    $sql  = "SELECT
+    article_id,
+    cu.user_id,
+    name,
+    title,
+    sentence,
+    like_count,
+    creation_date
+FROM
+    article_list AS al
+INNER JOIN
+    current_users AS cu
+ON
+    cu.user_id = al.user_id
+WHERE
+    sentence LIKE '" . "%" . $search_word . "%" . "'
+ORDER BY
+    creation_date DESC
+;";
     $stmt = $this->pdo->prepare($sql);
     $stmt->execute();
     $search_articles = $stmt->fetchAll();
     return  $search_articles;
   }
 }
+// "select  *  from  article_list join current_users on article_list.user_id = current_users.user_id where sentence like '" . "%" . $search_word . "%" . "' order by article_list.article_id desc";
