f0a97c270a7ae211d5a7356e97e7f64c3ca8c95e
Ishii Yuuki <kd1329330@st.kobedenshi.ac.jp>
2023-05-02 10:23:33 +0900

    topへ

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
 <!-- footer部分 -->
+<div id="retop">
+	<a href="#" class="retop1">TOP</a>
+</div>
 <footer>
   <p class="footer-text">Kobe Denshi Channel</p>
 </footer>
--- search/search_form.php
+++ search/search_form.php
@@ -6,6 +6,6 @@ if (isset($_SESSION['search_error'])) {
 ?>
 
 <form class="search" method="GET" action=<?= $search_php ?>?q=>
-    <input type="text" class="search_text" name="q" placeholder="記事を検索">
+    <input type="text" class="search_text" maxlength="50" name="q" placeholder="記事を検索">
     <input type="submit" value="検索">
 </form>
