6e7f578abcfed90507b3efde2a3aae17b1aa3b86
UemuraKatsuki <kd1330249@st.kobedenshi.ac.jp>
2023-05-02 13:15:45 +0900

    コード修正&コメント記入欄変更

--- article_comment/article_comment.php
+++ article_comment/article_comment.php
@@ -5,18 +5,15 @@ if(isset($_SESSION['article_comment_error'] )){
 }
 ?>
 
-<br><hr>
-<div class="outer">
-  <div class="inner">
-    <h2>新規コメント投稿<h2>
-  </div>
-</div>
+<br>
 
 <link rel="stylesheet" href="../css/article_post.css">
 
+<div class="anser-show">
 <form method="POST" action="../article_comment/article_comment_db.php">
   <input type="hidden" name="article_comment_id" value=<?= $_SESSION['article_comment_id']?>>
   <h1 class="comment_left">コメント入力</h1>
   <textarea class="comment-text" name="comment" placeholder="コメントを入力" maxlength="400" required></textarea>
   <input type="submit" value="送信" class="comment_button">
 </form>
+</div>
